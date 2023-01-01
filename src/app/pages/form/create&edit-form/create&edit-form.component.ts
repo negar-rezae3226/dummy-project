0acfb76ec32c629b0725e1e49b42bd140532e3fd
Negar-home <negarrezai3226@gmail.com>
2023-01-02 00:18:15 +0330

    form change

--- src/app/pages/form/create&edit-form/create&edit-form.component.ts
+++ src/app/pages/form/create&edit-form/create&edit-form.component.ts
@@ -4,28 +4,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'create-edit-form',
   templateUrl: './create&edit-form.component.html',
-  styleUrls: ['./create&edit-form.component.scss']
+  styleUrls: ['./create&edit-form.component.scss'],
 })
 export default class UserFormComponent implements OnInit {
+  hide = true;
   inputForm: FormGroup = new FormGroup('');
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
   ngOnInit(): void {
-
     this.inputForm = new FormGroup({
-      'firstName': new FormControl(null,Validators.required),
-      'lastName': new FormControl(null,Validators.required),
-      'userName': new FormControl(null,Validators.required),
-      'password': new FormControl(null,Validators.required),
-      'phone': new FormControl(null,[Validators.required, Validators.maxLength(12)]),
-      'email': new FormControl(null,[Validators.required,Validators.email]),
-      'age': new FormControl(null,[Validators.required,Validators.max(150),Validators.min(1)]),
-      'gender': new FormControl('' , Validators.required),
+      // firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      userName: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(12),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      age: new FormControl(null, [
+        Validators.required,
+        Validators.max(150),
+        Validators.min(1),
+      ]),
+      gender: new FormControl('', Validators.required),
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.inputForm);
-    
   }
-
 }
