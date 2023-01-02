4d15458a887b1e9048642b7f53e982a2f1ab6712
Negar-home <negarrezai3226@gmail.com>
2023-01-03 01:20:29 +0330

    change button in form

--- src/app/pages/form/create&edit-form/create&edit-form.component.ts
+++ src/app/pages/form/create&edit-form/create&edit-form.component.ts
@@ -17,69 +17,39 @@ export default class UserFormComponent implements OnInit {
     gender: '',
     password: '',
     username: '',
-    id: 0
+    id: 0,
   };
-
-
 
   hide = true;
   inputForm: FormGroup = new FormGroup('');
-  emailFormControl = new FormControl('', [
-  ]);
 
-  constructor(private addUser: UsersService) { }
+  constructor(private addUser: UsersService) {}
 
   ngOnInit(): void {
-    // this.inputForm = new FormGroup({
-    // firstName: new FormControl(null, Validators.required),
-    // lastName: new FormControl(null, Validators.required),
-    // userName: new FormControl(null, Validators.required),
-    // password: new FormControl(null, Validators.required),
-    // phone: new FormControl(null, [
-    //   Validators.required,
-    //   Validators.maxLength(12),
-    // ]),
-    // email: new FormControl(null, [Validators.required, Validators.email]),
-    // age: new FormControl(null, [
-    //   Validators.required,
-    //   Validators.max(150),
-    //   Validators.min(1),
-    // ]),
-    // gender: new FormControl('', Validators.required),
-    // });
+    this.inputForm = new FormGroup({
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    userName: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(12),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.max(150),
+      Validators.min(1),
+    ]),
+    gender: new FormControl('', Validators.required),
+    });
   }
-
-  onAddUserLastName(value: string) {
-    this.user.firstName = value;
-  }
-
-  onAddUserFirstName(value: string) {
-    this.user.lastName = value;
-  }
-
-  onAddUserUsername(value: string) {
-    this.user.username = value;
-  }
-
-  onAddUserPhone(value: string) {
-    this.user.phone = value;
-  }
-
-  onAddUserPassword(value: string) {
-    this.user.password = value;
-  }
-
-  onAddUserEmail(value: string) {
-    this.user.email = value;
-  }
-
-  
-
 
   onSubmit() {
-
     this.addUser.addNewUser(this.user);
-    this.addUser.getAllUsers();
-
+  }
+  blurEvent(event: any) {
+    this.user.gender=event.target.value;
   }
 }
+
