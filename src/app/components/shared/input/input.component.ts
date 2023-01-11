b177fedf0fa4e18f6113eb24e3b85e70d36b6308
Negar Rezaei <N.Rezaei@pishgam.local>
2023-01-11 16:44:43 +0330

    change user table style

--- src/app/components/shared/input/input.component.ts
+++ src/app/components/shared/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, DoCheck, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/models/user.interface';
 
@@ -8,16 +8,18 @@ import { User } from 'src/app/models/user.interface';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
+export class InputComponent implements DoCheck {
 
 
   @Input() labelInput = '';
+  @Input() formName = '';
   @Input() formControlInput = '';
   @Input() inputType: "text" | "number" | "password" | "email" = "text";
   @Input() inputFormControl: FormControl | any;
   @Input() form: FormGroup | any;
   @Input() value: any = "";
-
+  hide: boolean = true;
+  hideIcon: boolean = false;
 
   @Output() inputValue = new EventEmitter<any>();
 
@@ -29,9 +31,20 @@ export class InputComponent {
 
   onEmitValue() {
     this.inputValue.emit(this.value);
+    if (this.value && this.formName == 'password') {
+      
+      this.hideIcon=true;
+    }
+
   }
 
 
+  ngDoCheck(): void {
+
+
+
+    }
+
+
+
 }
-
-
